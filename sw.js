1b6f8ff6fddf6dd43f764b4f70dc8255df736c61
Gilles Vieira <vieira.gille@gmail.com>
2026-03-05 21:09:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "marathon-plan-v1";
+
+const urls = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(urls)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return r || fetch(e.request);
+    }),
+  );
+});
